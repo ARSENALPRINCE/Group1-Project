1743da8fa1639fd4aa7a628c31f3794ea98589d6
ARSENALPRINCE <nnamaniprince5@gmail.com>
2025-02-27 03:49:57 -0800

    first commit

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let x = document.getElementById("nav-link-container");
 let cl = document.getElementById("closeBtn");
 
 function toggleMenu() {
-    cl.style.display = "block"
-    x.style.display = "block";
-    x.style.width = "250px";
-    x.style.position = "fixed";
-    x.style.zIndex = "1";
-    x.style.height = "550px";
-    x.style.top = "0";
-    x.style.left = "0";
+    cl.style.display = "block"// Show the close button//
+    x.style.display = "block";// Show the menu//
+    x.style.width = "250px";// Set the menu width//
+    x.style.position = "fixed";// Fix the menu position on the screen//
+    x.style.zIndex = "1";// Bring the menu to the front//
+    x.style.height = "550px";// Set menu height//
+    x.style.top = "0";// Align to the top//
+    x.style.left = "0";// Align to the left//
     x.style.color = "#fff"
-    x.style.backgroundColor = "#fafafa";
-    x.style.overflowX = "hidden";
-    x.style.transition = "0.5s";
-    x.style.paddingTop = "60px";
+    x.style.backgroundColor = "#fafafa ";// Set background color//
+    x.style.overflowX = "hidden"; // Prevent horizontal scrolling//
+    x.style.transition = "0.5s"; // Smooth animation effect//
+    x.style.paddingTop = "60px"; // Smooth animation effect//
 }
 
 function closeNav() {
